318e8bb4f02b94a2d3bf20df3b175efe86f9b4f6
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-26 01:33:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// when deciding on a snack, you think, "Am i hungry?" If yes, you eat an apple; If not , you drink water.
+let isHungry = true;
+let snack = isHungry ? "apple" : "water";
+console.log(`You should have some ${snack}.`);
+let isFeelingHungry = false;
+let snack1 = isFeelingHungry ? "apple" : "water";
+console.log(`You should have some ${snack1}.`);
+let age = 17;
+let message = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,13 @@
+// when deciding on a snack, you think, "Am i hungry?" If yes, you eat an apple; If not , you drink water.
+let isHungry = true;
+let snack = isHungry? "apple" : "water";
+console.log(`You should have some ${snack}.`);
+
+let isFeelingHungry = false;
+let snack1 = isFeelingHungry? "apple" : "water";
+console.log(`You should have some ${snack1}.`);
+
+
+let age: number = 17;
+let message: string = (age>=18)? "You are an adult" : "You are a minor";
+console.log(message);
